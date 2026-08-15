a4affff71bfe927160ea75f31028cd0f073b7cc0
mjmoorman03 <90151067+mjmoorman03@users.noreply.github.com>
2026-08-14 21:10:08 -0400

    make driver two distinct loops

--- src/driver.sh
+++ src/driver.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
-# we really should make this spawn two processes, or at least spawn one child
-# such that we can have the api call fetch and write in the background
-# every 30s or something
-# and then we have the python one redisplay every 5 seconds
-# (this is somewhat necessary bc the python code must be continuously 
-#  running for the display to remain active)
+# kills the $CHILD_PID process when run
+cleanup() {
+    kill "$CHILD_PID" 2>/dev/null
+}
+trap cleanup EXIT SIGINT SIGTERM
 
-. ./.venv/bin/activate
+# python child background process to continuously paint screen anew
+(
+    . ./.venv/bin/activate
+    while true
+    do
+        python3 src/display.py
+    done
+) &
+CHILD_PID=$!
 
+# parent process, fetches continuously
 while true
 do 
     npx tsx src/mtaTrainTimes.ts
-    python3 src/display.py
-    sleep 5
+    sleep 30
 done
